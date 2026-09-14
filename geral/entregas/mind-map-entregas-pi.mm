49f0dc5da94eadf2b5de48a24151ea9831c40a5e
Haimon Vieira <81303638+haimonvieira@users.noreply.github.com>
2026-09-14 19:58:33 -0300

    Add files via upload

---
+++ geral/entregas/mind-map-entregas-pi.mm
@@ -0,0 +1,205 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1648433853499" ID="ID_1395705280" MODIFIED="1770732020970">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p style="text-align: center">
+      <b>Projeto Integrador III</b><br />Objetivo: Back-End II<br />DSM3 | Disc.Chave: GAPS
+    </p>
+    <p style="text-align: center">
+      Mentor: Prof. Ramon
+    </p>
+    <p style="text-align: center">
+      v.1.2
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1648433926905" ID="ID_1415945174" MODIFIED="1739657154076" POSITION="right" TEXT="Artigo Cient&#xed;fico">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1648591656584" ID="ID_1152596937" MODIFIED="1708893442783" TEXT="Respons&#xe1;vel: Orientador">
+<icon BUILTIN="info"/>
+</node>
+<node COLOR="#0033ff" CREATED="1648591670583" ID="ID_448977211" MODIFIED="1739714829114" STYLE="fork" TEXT="O que fazer? - &#xc9; a documenta&#xe7;&#xe3;o do PI">
+<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="messagebox_warning"/>
+</node>
+<node CREATED="1648591720154" ID="ID_767503612" MODIFIED="1739657154618" TEXT="Se&#xe7;&#xf5;es">
+<node CREATED="1739658015986" ID="ID_179641034" MODIFIED="1739739027322" TEXT="Introdu&#xe7;&#xe3;o, Objetivo e Estado da Arte">
+<font BOLD="true" ITALIC="true" NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1648591795237" ID="ID_1020392366" MODIFIED="1739739027322" TEXT="Metodologia">
+<font BOLD="true" ITALIC="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1648727068984" ID="ID_445175759" MODIFIED="1739657969578">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <font size="2">Discorra sobre o m&#233;todo que ser&#225; desenvolvido para propor a solu&#231;&#227;o do problema delineado no Objetivo. Recomenda-se a utiliza&#231;&#227;o de tecnologias e fundamentos de Intelig&#234;ncia Artificial, Internet das Coisas (IoT), Computa&#231;&#227;o em Nuvem, al&#233;m de Algoritmos e Estruturas de Dados de forma abrangente. A efici&#234;ncia do m&#233;todo deve ser obrigatoriamente mensurada por meio da An&#225;lise de Algoritmos. Al&#233;m disso, &#233; imprescind&#237;vel a elabora&#231;&#227;o de um fluxograma que ilustre o funcionamento do m&#233;todo desenvolvido.</font>
+  </body>
+</html></richcontent>
+</node>
+</node>
+</node>
+<node COLOR="#cc3300" CREATED="1648591895106" ID="ID_115586979" MODIFIED="1739657154602" TEXT="Importante!">
+<icon BUILTIN="button_ok"/>
+<node CREATED="1648639164365" ID="ID_461566659" MODIFIED="1755707209174" TEXT="Utilize o template padr&#xe3;o em Latex"/>
+<node CREATED="1690833966034" ID="ID_1950732156" MODIFIED="1739791291583" TEXT="A primeira vers&#xe3;o do artigo cient&#xed;fico dever&#xe1; ser entregue no 1&#xba; Bimestre"/>
+</node>
+</node>
+<node CREATED="1648433980945" ID="ID_971588175" MODIFIED="1739657119394" POSITION="right" TEXT="Artefatos do Projeto de Software">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1648591312787" ID="ID_177080595" MODIFIED="1708904466371" TEXT="Respons&#xe1;vel: Orientador">
+<icon BUILTIN="info"/>
+</node>
+<node COLOR="#0033ff" CREATED="1648591325373" ID="ID_285218129" MODIFIED="1755708433849" STYLE="fork" TEXT="O que fazer? - Anexar Documentos e Resultados obtidos no semestre atual e anteriores, sempre atualizados">
+<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="messagebox_warning"/>
+</node>
+<node CREATED="1648591364178" ID="ID_343372346" MODIFIED="1739657154661" TEXT="Artefatos">
+<node CREATED="1708899882652" ID="ID_1758449333" MODIFIED="1708899887798" TEXT="Di&#xe1;rio de Bordo"/>
+<node CREATED="1648591474496" ID="ID_1356625287" MODIFIED="1739653250749" TEXT="Diagrama de Caso de Uso, Classes e Objetos"/>
+<node CREATED="1648591386595" ID="ID_1520582450" MODIFIED="1739653279578" TEXT="Diagramas de Banco de Dados"/>
+<node CREATED="1708899544077" ID="ID_126296501" MODIFIED="1739653288802" TEXT="Canvas"/>
+<node CREATED="1708899578876" ID="ID_1767010818" MODIFIED="1723936818357" TEXT="Diagrama e Especifica&#xe7;&#xf5;es da Infraestrutura da Rede"/>
+<node CREATED="1708899621437" ID="ID_1840690891" MODIFIED="1708899637231" TEXT="UI de Alta Fidelidade (Figma)"/>
+<node CREATED="1739653332204" ID="ID_1771145681" MODIFIED="1739653333862" TEXT="Interfaces do Website"/>
+<node CREATED="1739656417594" ID="ID_1477346606" MODIFIED="1739658315798" TEXT="Pitch">
+<node CREATED="1739658319250" ID="ID_495556709" MODIFIED="1739658321645" TEXT="IA Generativa"/>
+<node CREATED="1739658260818" ID="ID_9231126" MODIFIED="1739658356078" TEXT="Tempo de 1 a 2 minutos"/>
+<node CREATED="1739658296690" ID="ID_121343676" MODIFIED="1739658401776" TEXT="Legendas em Portugu&#xea;s + Ingl&#xea;s"/>
+</node>
+<node CREATED="1739657030345" ID="ID_632058570" MODIFIED="1739657043033" TEXT="An&#xe1;lise SWOT"/>
+</node>
+<node COLOR="#cc3300" CREATED="1648591503829" ID="ID_368377489" MODIFIED="1723990783226" TEXT="Importante!">
+<icon BUILTIN="button_ok"/>
+<node CREATED="1648731428173" ID="ID_1871094737" MODIFIED="1708899734213" TEXT="Os artefatos dever&#xe3;o ser atualizados sempre que o projeto sofrer altera&#xe7;&#xf5;es "/>
+<node CREATED="1708899784572" ID="ID_694776305" MODIFIED="1739657353048" TEXT="Este documento n&#xe3;o deve ser anexado ao Artigo Cient&#xed;fico"/>
+<node CREATED="1648639164365" ID="ID_6868480" MODIFIED="1739658595156" TEXT="Utilize o template padr&#xe3;o (docx)"/>
+</node>
+</node>
+<node CREATED="1648433992935" ID="ID_1785712511" MODIFIED="1739654544424" POSITION="left" TEXT="Projeto de Software">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1648590745192" ID="ID_1512213092" MODIFIED="1739794547353" TEXT="Disciplinas: IHC + TP2 + DW3">
+<icon BUILTIN="info"/>
+</node>
+<node COLOR="#0033ff" CREATED="1648590753655" ID="ID_1106265221" MODIFIED="1739794329210" STYLE="fork" TEXT="O que fazer? - O projeto deve ser desenvolvido seguindo os princ&#xed;pios da &#xa;Programa&#xe7;&#xe3;o Orientada a Objetos e, preferencialmente, ser multiplataforma &#xa;(Desktop, Web e/ou Mobile). Al&#xe9;m disso, deve incorporar fundamentos te&#xf3;ricos &#xa;e/ou pr&#xe1;ticos em Computa&#xe7;&#xe3;o em Nuvem, Internet das Coisas e/ou &#xa;Intelig&#xea;ncia Artificial. Nesta vers&#xe3;o, &#xe9; fundamental evidenciar as boas pr&#xe1;ticas &#xa;de desenvolvimento de software, aplicando as t&#xe9;cnicas ensinadas &#xa;na disciplina de IHC.">
+<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="messagebox_warning"/>
+</node>
+<node CREATED="1648590786251" ID="ID_1273887857" MODIFIED="1739657154645" TEXT="Tecnologias">
+<node CREATED="1648590793485" ID="ID_1784495434" MODIFIED="1739895372884" TEXT="Java, Node.js, Python, React Native e/ou Kotlin"/>
+<node CREATED="1739658932850" ID="ID_791235794" MODIFIED="1739658964204" TEXT="MySQL, PostgreSQL, Oracle e/ou MongoDB"/>
+<node CREATED="1739658974929" ID="ID_1080207540" MODIFIED="1739659008554" TEXT="Cloud Computing, IA e/ou IoT"/>
+</node>
+<node COLOR="#cc3300" CREATED="1648590897192" ID="ID_135823318" MODIFIED="1739657154645" TEXT="Importante!">
+<icon BUILTIN="button_ok"/>
+<node CREATED="1648593870325" ID="ID_1568409257" MODIFIED="1739650383888" TEXT="Enviar ao Github"/>
+<node CREATED="1648731428173" ID="ID_1568190113" MODIFIED="1708893048590" TEXT="As corre&#xe7;&#xf5;es ocorrer&#xe3;o dentro da disciplina "/>
+</node>
+</node>
+<node CREATED="1648433980945" ID="ID_703305248" MODIFIED="1755712934243" POSITION="left" TEXT="Pesquisa de Usu&#xe1;rio">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1648591312787" ID="ID_1417053277" MODIFIED="1755708570127" TEXT="Disciplina: IHC">
+<icon BUILTIN="info"/>
+</node>
+<node COLOR="#0033ff" CREATED="1648591325373" ID="ID_230460519" MODIFIED="1688491892434" STYLE="fork" TEXT="O que fazer? - Pesquisa de campo, utilizando os m&#xe9;todos aplicados em aula. ">
+<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="messagebox_warning"/>
+</node>
+<node COLOR="#cc3300" CREATED="1648591234502" ID="ID_39507515" MODIFIED="1648592774833" TEXT="Importante!">
+<icon BUILTIN="button_ok"/>
+<node CREATED="1648590935441" ID="ID_1360284064" MODIFIED="1755712978594" STYLE="fork" TEXT="A equipe deve ir a campo em busca de dados referentes a &#xa;User Research, como a cria&#xe7;&#xe3;o de personas e ao levantamento &#xa;de requisitos de IHC"/>
+<node CREATED="1648590935441" ID="ID_92583319" MODIFIED="1739725199227" STYLE="fork" TEXT="&#xc9; obrigat&#xf3;rio evidenciar a pesquisa com artefatos multim&#xed;dia"/>
+</node>
+</node>
+<node CREATED="1648433980945" ID="ID_1707900460" MODIFIED="1739652756273" POSITION="left" TEXT="Banco de Dados">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1648591312787" ID="ID_305119011" MODIFIED="1739656074091" TEXT="Disciplina: BDNR">
+<icon BUILTIN="info"/>
+</node>
+<node COLOR="#0033ff" CREATED="1648591325373" ID="ID_1172797474" MODIFIED="1739656091231" STYLE="fork" TEXT="O que fazer? - A documenta&#xe7;&#xe3;o e os fundamentos de Banco de Dados">
+<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="messagebox_warning"/>
+</node>
+<node CREATED="1648591364178" ID="ID_454735529" MODIFIED="1739657154634" TEXT="Tecnologias">
+<node CREATED="1648591474496" ID="ID_1927539123" MODIFIED="1739653885334" TEXT="Modelo Relacional ou N&#xe3;o-Relacional"/>
+</node>
+<node COLOR="#cc3300" CREATED="1648591503829" ID="ID_780329578" MODIFIED="1739657154634" TEXT="Importante!">
+<icon BUILTIN="button_ok"/>
+<node CREATED="1648731428173" ID="ID_1180490088" MODIFIED="1708889378676" TEXT="As corre&#xe7;&#xf5;es ocorrer&#xe3;o dentro da disciplina "/>
+</node>
+</node>
+<node CREATED="1648433966926" ID="ID_750717560" MODIFIED="1739654765472" POSITION="left" TEXT="Documenta&#xe7;&#xe3;o">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1648591125922" ID="ID_900334828" MODIFIED="1677974259031" TEXT="Disciplina: GAPS">
+<icon BUILTIN="info"/>
+</node>
+<node COLOR="#0033ff" CREATED="1648591138590" ID="ID_1198137503" MODIFIED="1739654888376" STYLE="fork" TEXT="O que fazer? - Organizar as atividades com &#xc1;gil e desenvolver a An&#xe1;lise SWOT ">
+<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="messagebox_warning"/>
+</node>
+<node CREATED="1648591183725" ID="ID_890199964" MODIFIED="1739657154634" TEXT="Tecnologias">
+<node CREATED="1648591192335" ID="ID_1608576086" MODIFIED="1678273264694" TEXT="Scrum"/>
+<node CREATED="1661710002386" ID="ID_1036712142" MODIFIED="1739654875123" TEXT="SWOT"/>
+</node>
+<node COLOR="#cc3300" CREATED="1648591234502" ID="ID_416275859" MODIFIED="1739657154634" TEXT="Importante!">
+<icon BUILTIN="button_ok"/>
+<node CREATED="1648731428173" ID="ID_1991072043" MODIFIED="1708893097511" TEXT="As corre&#xe7;&#xf5;es ocorrer&#xe3;o dentro da disciplina  "/>
+</node>
+</node>
+<node CREATED="1648434013822" ID="ID_179385132" MODIFIED="1739739384758" POSITION="left" TEXT="Apresenta&#xe7;&#xe3;o e Requisitos">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1648592190681" ID="ID_122036706" MODIFIED="1739658127565" TEXT="Respons&#xe1;vel: GAPS">
+<icon BUILTIN="info"/>
+</node>
+<node COLOR="#0033ff" CREATED="1648592211803" ID="ID_1653608100" MODIFIED="1739791596878" STYLE="fork" TEXT="O que fazer? Uma apresenta&#xe7;&#xe3;o resumida de todos os itens deste Mind Map&#xa; na Disciplina Chave">
+<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="messagebox_warning"/>
+</node>
+<node COLOR="#cc3300" CREATED="1648592240515" ID="ID_1639875523" MODIFIED="1648592774833" TEXT="Importante!">
+<icon BUILTIN="button_ok"/>
+<node CREATED="1648592247166" ID="ID_1481359838" MODIFIED="1724003711355" STYLE="fork" TEXT="Tempo de 10 a 11 minutos"/>
+<node CREATED="1648592263553" ID="ID_143305863" MODIFIED="1739650082071" STYLE="fork" TEXT="Seguir o Manual de Semin&#xe1;rios, NDE-DSM-07-2024"/>
+<node CREATED="1648592382302" ID="ID_802528377" MODIFIED="1739658144842" STYLE="fork" TEXT="Dever&#xe1; ser apresentado PI em funcionamento"/>
+<node CREATED="1723987877259" ID="ID_1548008798" MODIFIED="1739792282776" TEXT="Obrigat&#xf3;ria a exposi&#xe7;&#xe3;o do projeto + banner (atualizado) na FTX ou HubTec"/>
+<node CREATED="1648593647207" ID="ID_1576713460" MODIFIED="1739792381117" STYLE="fork" TEXT="Depositar o PI completo via Teams, em 14 dias antes da apresenta&#xe7;&#xe3;o (docx)"/>
+<node CREATED="1740173747817" ID="ID_856370993" MODIFIED="1740173982318" TEXT="Enviar registros (apenas em fotos) das atividades realizadas &#xa;pela equipe no semestre atual (no m&#xe1;ximo 5)"/>
+<node CREATED="1708891824234" ID="ID_1852537228" MODIFIED="1739729990214" TEXT="O projeto dever&#xe1; ser submetido na FETEPS"/>
+</node>
+</node>
+<node CREATED="1661710585645" ID="ID_1928225374" MODIFIED="1755708043227" POSITION="right" TEXT="Escola de Inovadores">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1661710616756" ID="ID_1500821658" MODIFIED="1708893352206" TEXT="Respons&#xe1;vel: Orientador">
+<icon BUILTIN="info"/>
+</node>
+<node COLOR="#0033ff" CREATED="1648590859913" ID="ID_1882341806" MODIFIED="1755708060789" STYLE="fork">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      O que fazer? - Cadastrar o seu projeto na plataforma da
+    </p>
+    <p>
+      Escola de Inovadores da Ag&#234;ncia INOVA Paula Souza
+    </p>
+  </body>
+</html></richcontent>
+<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="messagebox_warning"/>
+</node>
+<node COLOR="#cc3300" CREATED="1648591041243" ID="ID_1199715191" MODIFIED="1648592774833" TEXT="Importante!">
+<icon BUILTIN="button_ok"/>
+<node CREATED="1648591045557" ID="ID_1149861462" MODIFIED="1688491280647" STYLE="fork" TEXT="&#xc9; obrigat&#xf3;ria a participa&#xe7;&#xe3;o de ao menos um integrante &#xa;de cada equipe na Escola de Inovadores"/>
+<node CREATED="1661710967748" ID="ID_1998256314" MODIFIED="1688491242697" TEXT="&#xc9; responsabilidade da equipe a participa&#xe7;&#xe3;o nas aulas e a &#xa;elabora&#xe7;&#xe3;o do Canvas e do Pitch durante todo o per&#xed;odo do curso"/>
+<node CREATED="1677947796859" ID="ID_1734787223" MODIFIED="1755708403996" TEXT="Poder&#xe1; ser utilizado o mesmo Canvas e Pitch j&#xe1; desenvolvidos no semestre anterior"/>
+</node>
+</node>
+</node>
+</map>
